d425ca0cf4c0fb2f6bf0c793e71df2b246d827c7
Armando Roman <armandoroman1016@gmail.com>
2019-06-26 22:21:27 -0700

    javascript functionality added

--- javascript/index.js
+++ javascript/index.js
@@ -12,7 +12,7 @@ class Panel{
         this.closeBtn.classList.toggle('hide-btn')
         this.openBtn.classList.toggle('hide-btn')
         if (this.openBtn.classList.contains('hide-btn')){
-        this.panelContent.setAttribute('style', 'display:flex', 'align-items:center', 'justify-content:space-between')
+        this.panelContent.setAttribute('style', 'display:flex', 'align-items:center', 'justify-content:space-evenly')
         }else{
             this.panelContent.style.display = 'none';
         }
@@ -21,8 +21,42 @@ class Panel{
 
 const panels = document.querySelectorAll('.panel')
 panels.forEach((panel) => new Panel(panel))
-console.log(panels)
 
 const inputs = document.querySelectorAll('input')
 const message = document.querySelectorAll('textarea')
+
+// increase size of forms when focused
+inputs.forEach((input) => input.addEventListener('focus', (event) => {
+    event.target.setAttribute('style', 'transform:scale(1.09)')
+},true))
+
+inputs.forEach((input) => input.addEventListener('blur', (event) => {
+    event.target.setAttribute('style', 'transform:scale(1.00)')
+},true))
+
+message.forEach((message) => message.addEventListener('focus', (event) => {
+    event.target.setAttribute('style', 'transform:scale(1.02)')
+},true))
+
+message.forEach((message) => message.addEventListener('blur', (event) => {
+    event.target.setAttribute('style', 'transform:scale(1.00)')
+},true))
+
+let urlStr = document.location.href
+let urlArr = urlStr.split('/')
+let pageHREF = urlArr[urlArr.length-1]
+
+if (pageHREF !== ''){
+    let menuItems = document.querySelectorAll('.nav-link')
+    for(let i = 0; i < menuItems.length; i++){
+        let currentURL = (menuItems[i].getAttribute('href'))
+            menuItems[i].classList.remove('active')
+        if (currentURL === pageHREF){
+            menuItems[i].classList.add('active')
+        }
+    }
+}
+
+
+
 
