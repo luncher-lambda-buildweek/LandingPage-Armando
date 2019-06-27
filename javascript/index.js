2b637c6f35696af15e79f1d7ea5597e13db3dc95
Armando Roman <armandoroman1016@gmail.com>
2019-06-27 12:03:48 -0700

    added fuunctionality to carousel

--- javascript/index.js
+++ javascript/index.js
@@ -62,9 +62,9 @@ const track = document.querySelector('.carousel_track')
 const slides = Array.from(document.querySelectorAll('.carousel_slide'))
 const nextButton = document.querySelector('.carousel-button-right')
 const prevButton = document.querySelector('.carousel-button-left')
-const dotsNav = document.querySelectorAll('.carousel-nav')
-const dots = document.querySelectorAll('.carousel-indicator')
-
+const dotsNav = document.querySelector('.carousel-nav')
+const dots = dotsNav.querySelectorAll('.carousel-indicator')
+console.log(dots)
 const slideWidth = slides[0].getBoundingClientRect().width
 
 const setSlidePosition = (slide, index) => {
@@ -79,18 +79,69 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
     currentSlide.classList.remove('current-slide')
     targetSlide.classList.add('current-slide')
 }
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide')
+    targetDot.classList.add('current-slide')
+}
+
+const displayArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0){
+        prevButton.classList.add('is-hidden')
+        nextButton.classList.remove('is-hidden')
+    }else if (targetIndex === slides.length - 1){
+        prevButton.classList.remove('is-hidden')
+        nextButton.classList.add('is-hidden')
+    }else if (targetIndex > 0 && targetIndex < slides.length - 1){
+        prevButton.classList.remove('is-hidden')
+        nextButton.classList.remove('is-hidden')
+}
+}
 //move slide to the right
 nextButton.addEventListener('click', (event) =>{
     const currentSlide = track.querySelector('.current-slide')
     const nextSlide = currentSlide.nextElementSibling
 
+    const currentDot = dotsNav.querySelector('.current-slide')
+    const nextDot = currentDot.nextElementSibling
+    const nextIndex = slides.findIndex((slide) => slide === nextSlide) 
+    
     moveToSlide(track, currentSlide, nextSlide)
+    updateDots(currentDot, nextDot);
+
+    displayArrows(slides, prevButton, nextButton, nextIndex)
+
 })
 
 prevButton.addEventListener('click', (event) => {
     const currentSlide = track.querySelector('.current-slide')
     const prevSlide = currentSlide.previousElementSibling
 
+    const currentDot = dotsNav.querySelector('.current-slide')
+    const prevDot = currentDot.previousElementSibling
+    const prevIndex = slides.findIndex((slide) => slide === prevSlide) 
+
     moveToSlide(track, currentSlide, prevSlide)
+    updateDots(currentDot, prevDot)
+
+    displayArrows(slides, prevButton, nextButton, prevIndex)
+
+})
+
+dotsNav.addEventListener('click', (event) =>{
+    const targetDot = event.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide')
+    const currentDot =  dotsNav.querySelector('.current-slide')
+    const targetIndex = Array.from(dots).findIndex(dot => dot === targetDot)
+    const targetSlide = slides[targetIndex]
+
+    moveToSlide(track, currentSlide, targetSlide);
+
+    updateDots(currentDot, targetDot)
+    
+    displayArrows(slides, prevButton, nextButton, targetIndex)
 })
 
